9e4955aa32f5c3978d88623f8fc1f469a8eef678
Evgeny Kovtunenko <ekovtunenko@iflex.ru>
2016-10-06 17:31:23 +0300

    переход с mac на win10

--- app/src/main/java/org/investsoft/bazar/ui/LoginActivity.java
+++ app/src/main/java/org/investsoft/bazar/ui/LoginActivity.java
@@ -32,7 +32,6 @@ public class LoginActivity extends AsyncActivity implements LoginView, OnClickLi
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         if (presenter == null) {
             presenter = new LoginPresenter();
         }
